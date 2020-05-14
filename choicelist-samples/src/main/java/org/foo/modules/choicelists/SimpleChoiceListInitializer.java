e2c0b3d638356d50aef09d3ee1633bdeda59553e
tdraier <tdraier@jahia.com>
2020-05-14 10:37:52 +0200

    Add renderer implementation

--- choicelist-samples/src/main/java/org/foo/modules/choicelists/SimpleChoiceListInitializer.java
+++ choicelist-samples/src/main/java/org/foo/modules/choicelists/SimpleChoiceListInitializer.java
@@ -1,25 +1,28 @@
 package org.foo.modules.choicelists;
 
-import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.*;
+import org.jahia.services.content.JCRPropertyWrapper;
 import org.jahia.services.content.nodetypes.ExtendedPropertyDefinition;
 import org.jahia.services.content.nodetypes.initializers.ChoiceListValue;
 import org.jahia.services.content.nodetypes.initializers.ModuleChoiceListInitializer;
+import org.jahia.services.content.nodetypes.renderer.AbstractChoiceListRenderer;
+import org.jahia.services.content.nodetypes.renderer.ModuleChoiceListRenderer;
+import org.jahia.services.render.RenderContext;
+import org.jahia.utils.Patterns;
 import org.osgi.service.component.annotations.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import javax.jcr.RepositoryException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * A simple configurable ChoiceList initializer
  */
-@Component(service = ModuleChoiceListInitializer.class, scope=ServiceScope.BUNDLE)
-public class SimpleChoiceListInitializer implements ModuleChoiceListInitializer {
+@Component(service = {ModuleChoiceListInitializer.class, ModuleChoiceListRenderer.class}, scope=ServiceScope.BUNDLE)
+public class SimpleChoiceListInitializer extends AbstractChoiceListRenderer implements ModuleChoiceListRenderer, ModuleChoiceListInitializer {
 
     private String key;
-    private List<String> values;
+    private LazyDynaMap values = new LazyDynaMap();
 
     @Activate
     public void activate(Map<String, ?> props) {
@@ -40,12 +43,30 @@ public class SimpleChoiceListInitializer implements ModuleChoiceListInitializer 
         this.key = key;
     }
 
-    public void setValues(String values) {
-        this.values = Arrays.asList(values.split(","));
+    public DynaBean getValues() {
+        return values;
+    }
+
+    @Override
+    public String getStringRendering(RenderContext context, JCRPropertyWrapper propertyWrapper)
+            throws RepositoryException {
+        String value;
+        if (propertyWrapper.isMultiple()) {
+            value = propertyWrapper.getValues()[0].getString();
+        } else {
+            value = propertyWrapper.getValue().getString();
+        }
+        return getStringRendering(context, null, value);
+    }
+
+    @Override
+    public String getStringRendering(Locale locale, ExtendedPropertyDefinition extendedPropertyDefinition, Object o) throws RepositoryException {
+        return (String) values.get(o.toString());
     }
 
     @Override
     public List<ChoiceListValue> getChoiceListValues(ExtendedPropertyDefinition extendedPropertyDefinition, String s, List<ChoiceListValue> list, Locale locale, Map<String, Object> map) {
-        return values.stream().map(v -> new ChoiceListValue(v,v)).collect(Collectors.toList());
+        Set<Map.Entry<String,String>> set = values.getMap().entrySet();
+        return set.stream().map(entry -> new ChoiceListValue(entry.getValue(), entry.getKey())).collect(Collectors.toList());
     }
 }
