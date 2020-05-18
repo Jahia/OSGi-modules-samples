3d05b059be005dcef624368ca4f9c4b8067d5119
Yousria <ybenchadi@jahia.com>
2020-05-18 13:29:53 +0200

    Backlog 13684 (#11)
    add validator sample

---
+++ validator-samples/src/main/java/org/foo/modules/validators/CustomValidators.java
@@ -0,0 +1,49 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms &amp; Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
+package org.foo.modules.validators;
+
+import org.jahia.services.content.decorator.validation.JCRNodeValidatorDefinition;
+import org.osgi.service.component.annotations.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ *
+ * @author yousria
+ */
+@Component(service = JCRNodeValidatorDefinition.class)
+public class CustomValidators extends JCRNodeValidatorDefinition {
+    /**
+     * Register custom validator for generic content nodetype
+     *
+     * @return validators list
+     * @see org.foo.modules.validators.TestContent
+     * @see org.foo.modules.validators.TestNodeValidator
+     */
+    @Override
+    public Map<String, Class> getValidators() {
+        return Collections.singletonMap(TestContent.NODETYPE, TestNodeValidator.class);
+    }
+}
---
+++ validator-samples/src/main/java/org/foo/modules/validators/TestNodeValidator.java
@@ -0,0 +1,53 @@
+/*
+ * ==========================================================================================
+ * =                            JAHIA'S ENTERPRISE DISTRIBUTION                             =
+ * ==========================================================================================
+ *
+ *                                  http://www.jahia.com
+ *
+ * JAHIA'S ENTERPRISE DISTRIBUTIONS LICENSING - IMPORTANT INFORMATION
+ * ==========================================================================================
+ *
+ *     Copyright (C) 2002-2020 Jahia Solutions Group. All rights reserved.
+ *
+ *     This file is part of a Jahia's Enterprise Distribution.
+ *
+ *     Jahia's Enterprise Distributions must be used in accordance with the terms
+ *     contained in the Jahia Solutions Group Terms &amp; Conditions as well as
+ *     the Jahia Sustainable Enterprise License (JSEL).
+ *
+ *     For questions regarding licensing, support, production usage...
+ *     please contact our team at sales@jahia.com or go to http://www.jahia.com/license.
+ *
+ * ==========================================================================================
+ */
+package org.foo.modules.validators;
+
+import org.jahia.services.content.JCRNodeWrapper;
+import org.jahia.services.content.decorator.validation.JCRNodeValidator;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.osgi.service.component.annotations.Component;
+
+/**
+ *
+ * @author yousria
+ */
+public class TestNodeValidator implements JCRNodeValidator {
+
+    private JCRNodeWrapper node;
+
+    public TestNodeValidator(JCRNodeWrapper node) {
+        this.node = node;
+    }
+
+    @Length(min = 1000)
+    public String getLongValue() {
+        return node.getPropertyAsString("longValue");
+    }
+
+    @NotEmpty
+    public String getNotEmptyValue() {
+        return node.getPropertyAsString("notEmptyValue");
+    }
+}
