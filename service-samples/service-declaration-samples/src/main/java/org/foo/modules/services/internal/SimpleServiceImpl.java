46421289e56e69f55e0f5168d6b640146541dd22
Serge Huber <shuber@jahia.com>
2023-04-28 15:40:53 +0200

    ACADEMY-967 Add configuration to the service sample - Also fix CSRF by whitelisting the action - Change the README to update the curl request to use POST instead of GET

--- service-samples/service-declaration-samples/src/main/java/org/foo/modules/services/internal/SimpleServiceImpl.java
+++ service-samples/service-declaration-samples/src/main/java/org/foo/modules/services/internal/SimpleServiceImpl.java
@@ -1,13 +1,27 @@
 package org.foo.modules.services.internal;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.foo.modules.services.SimpleService;
 import org.osgi.service.component.annotations.Component;
 
-@Component(service = SimpleService.class)
+import java.util.Map;
+
+@Component(service = SimpleService.class, configurationPid = "org.foo.modules.services.simple")
 public class SimpleServiceImpl implements SimpleService {
+
+    private String userName = "World";
+    private String userHome = null;
+    public void activate(Map<String, ?> props) {
+        if (props.containsKey("userName")) {
+            userName = (String) props.get("userName");
+        }
+        if (props.containsKey("userHome")) {
+            userHome = (String) props.get("userHome");
+        }
+    }
 
     @Override
     public String sayHello() {
-        return "Hello Jahia!";
+        return "Hello "+userName+"!" + (userHome != null ? " Home directory=" + userHome : "");
     }
 }
