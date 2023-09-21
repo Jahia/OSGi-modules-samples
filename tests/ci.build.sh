fffdc59b5e08f7ecd97bedd9ec76cb53e7a85c25
Thomas Draier <tdraier@jahia.com>
2023-09-21 09:01:19 +0200

    renamed script

---
+++ tests/ci.build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script can be used to manually build the docker images necessary to run the tests
+# It should be executed from the tests folder
+
+source ./set-env.sh
+
+# It assumes that you previously built the module you're going to be testing
+#   and that the modules artifacts are located one level up
+
+if [[ ! -f .env ]]; then
+ cp .env.example .env
+fi
+
+source .env
+
+if [ ! -d ./artifacts ]; then
+  mkdir -p ./artifacts
+fi
+
+cd ..
+mvn install
+cd -
+
+find .. -name target -exec bash -c 'cp {}/*-SNAPSHOT.jar ./artifacts/' \;
+
+docker build -t ${TESTS_IMAGE} .
+
