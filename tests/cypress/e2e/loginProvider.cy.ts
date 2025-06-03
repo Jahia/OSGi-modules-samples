8399b280dc2125339fc51a18f8c9d2b6c3ce215f
Jérôme Blanchard <jblanchard@jahia.com>
2025-06-03 09:59:10 +0200

    test: fix nightly test broken on Jahia SN #59 (#61)

--- tests/cypress/e2e/loginProvider.cy.ts
+++ tests/cypress/e2e/loginProvider.cy.ts
@@ -1,3 +1,6 @@
+import {getJahiaVersion} from '@jahia/cypress';
+import {compare} from 'compare-versions';
+
 describe('Login/logout url provider', () => {
     it('should use custom login url when going to /start', function () {
         cy.visit('/start');
@@ -14,7 +17,16 @@ describe('Login/logout url provider', () => {
     it('should use custom login logout', function () {
         cy.login();
         cy.visit('/start');
-        cy.get('[role="primary-nav-control"]').click();
-        cy.contains('Sign out').click();
+        getJahiaVersion().then(jahiaVersion => {
+            console.log(jahiaVersion);
+            if (compare(jahiaVersion.release.replace('-SNAPSHOT', ''), '8.2.2', '<')) {
+                console.log('Test using old style primaryNavMenuButton');
+                cy.get('[role="primary-nav-control"]').click();
+                cy.contains('Sign out').click();
+            } else {
+                cy.get('[data-testid="primaryNavMenuButton"]').click();
+                cy.contains('Sign out').click();
+            }
+        });
     });
 });
